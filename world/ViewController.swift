2ee959c01886471921ccae6e04408d2545d668d1
2matt24 <maxbg.do284@gmail.com>
2025-10-06 23:36:51 -0400

    activate constraints

--- world/ViewController.swift
+++ world/ViewController.swift
@@ -6,8 +6,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
+        override func loadView() {
+            
+            mapView = MKMapView()
+            
+            view = mapView
+            
+            let segmentedControl
+            = UISegmentedControl (items: ["Standard","Hybrid","Satellite"])
+            segmentedControl.backgroundColor = UIColor.systemBackground
+            segmentedControl.selectedSegmentIndex = 0
+            
+            segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(segmentedControl)
+            
+            let topConstraint =
+                segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+            let leadingConstraint =
+                segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            let trailingConstraint =
+                segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            
+        
+            topConstraint.isActive = true
+            leadingConstraint.isActive = true
+            trailingConstraint.isActive = true
+            
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
